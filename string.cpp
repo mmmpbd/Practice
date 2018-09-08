646b655318f5f34f5c51099defd80ebf7a475894
mmmpbd <42223991+mmmpbd@users.noreply.github.com>
2018-09-07 21:06:34 -0400

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <string>
+using namespace std;
+
+/*int main(){
+char str[] = {'a','b','c','\0'};
+char str1[4];
+string my_string;
+cout << str<<endl;
+strcpy(str1,str);
+cout<<str1<<endl;
+for(int i = 0;i<5;i++){
+    my_string.push_back('a');
+}
+my_string.pop_back();
+for (auto it = my_string.begin(); it!=my_string.end(); it++){
+    cout << *it;
+}
+return 1;
+}
+int main(){
+    char str[] = {'m','a','n','z','u','r','\0'};
+    int i = 0;
+    while(str[i] != '\0'){
+        cout<<str[i];
+        i++;
+    }
+    string your_string;
+    for (int i = 0; i<5;i++){
+        your_string.push_back(i+'0');
+    }
+    for(auto ptr = your_string.begin();ptr != your_string.end();ptr++){
+        cout<< *ptr;
+    }
+    return 1;
+}*/
